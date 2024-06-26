06be0528b6291583f682c3377653790fbb6edf20
antonsouto <antonsouto97@gmail.com>
2024-06-26 08:30:37 +0200

    Lista simple completa con menu

--- ListaDoble/ListaDoble.cpp
+++ ListaDoble/ListaDoble.cpp
@@ -1,10 +1,133 @@
-#include"ListaDoble.h"
+#include "lista.h"
+#include <stdexcept>
 
+string uuid()
+{
+  string id = "";
+  for (int i = 0; i < 5; i++)
+  {
+    id += 'a' + rand() % 26;
+  }
+  return id;
+}
 
+P_Node insertBefore(P_Node const &it, int data)
+{
+  P_Node node = make_shared<Node>(Node{uuid(), data, it, it->prev});
+  it->prev->next = node;
+  it->prev = node;
+  return node;
+}
 
-P_Node push_back(P_Node const &head, int dato){
-    if(head!=nullptr){
-        P_Node node = make_shared<Node>(Node{uuid(), dato, nullptr, nullptr});
-        return node;
+/**
+ * Function to add a new data at the end of the list
+ * @param head first node of the list
+ * @param data the data of the new node
+ * @return first node of the list
+ */
+P_Node push_back(P_Node const &head, int data)
+{
+  if (!head)
+  {
+    P_Node node = make_shared<Node>(Node{uuid(), data, nullptr, nullptr});
+    return node;
+  }
+
+  auto it = head;
+  while (it->next)
+  {
+    it = it->next;
+  }
+
+  // estoy en el ultimo
+  P_Node node = make_shared<Node>(Node{uuid(), data, nullptr, it});
+  it->next = node;
+
+  return head;
+}
+
+P_Node push_front(P_Node const &head, int data)
+{
+  if (!head)
+  {
+    P_Node node = make_shared<Node>(Node{uuid(), data, nullptr, nullptr});
+    return node;
+  }
+
+  P_Node node = make_shared<Node>(Node{uuid(), data, head, nullptr});
+  return node;
+}
+
+P_Node push_at(P_Node const &head, int data, int pos)
+{
+  if (pos < 0)
+    throw std::out_of_range("Invalid position. Must be greater or equal than 0");
+  if (pos == 0)
+    return push_front(head, data);
+
+  if (!head && pos != 0)
+    throw std::out_of_range("Invalid position");
+
+  // sabemos que hay head y que la pos > 0
+  auto it = head;
+  int index{0};
+  while (it->next)
+  {
+    index++;
+    it = it->next;
+    if (pos == index)
+    {
+      P_Node node = insertBefore(it, data);
+      return head;
     }
+  }
+  throw std::out_of_range("Invalid position. Greater than list.");
+}
+P_Node push_in_order(P_Node const &head, int data, bool asc)
+{
+  if (!head)
+  {
+    P_Node node = make_shared<Node>(Node{uuid(), data, nullptr, nullptr});
+    return node;
+  }
+
+  auto it = head;
+  while (it->next)
+  {
+    auto condition = asc ? it->next->data < data : it->next->data > data;
+    if (condition)
+    {
+      it = it->next;
+    }
+    else
+    {
+      P_Node node = insertBefore(it, data);
+      return head;
+    }
+  }
+  push_back(head, data);
+  return head;
+}
+
+P_Node pop_back(P_Node &head)
+{
+  if (!head)
+    return nullptr;
+  auto it = head;
+
+  if (!head->next)
+  {
+    auto aux = head;
+    head = nullptr;
+    return aux;
+  }
+
+  while (it->next)
+  {
+    it = it->next;
+  }
+
+  it->prev->next = nullptr; // el anterior a mi es el nuevo ultimo
+  it->prev = nullptr;       // yo soy el ultimo y me salgo de la lista
+  return it;                // devuelvo el que era el ultimo
 }
--- ListaDoble/ListaDoble.h
+++ ListaDoble/ListaDoble.h
@@ -1,33 +1,30 @@
-#pragma once 
-#include<iostream>
-#include<memory>
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <memory>
 using namespace std;
 
+struct Node;
 
-//FUNCION QUE GENERA UN ID ALEATORIO
-string uuid()
+using P_Node = shared_ptr<Node>;
+
+struct Node
 {
-    string id = "";
-    for (int i = 0; i < 5; i++)
-    {
-        id += 'a' + rand() % 26;
-    }
-    return id;
-}
-
-//ESTRUCTURA DE CADA UNO DE LOS NODOS DE LA LISTA
-using P_Node = shared_ptr<int>;
-struct Node{
-    string id;
-    int data;
-    P_Node next;
-    P_Node prec;
+  string id;
+  int data;
+  P_Node next;
+  P_Node prev;
 };
 
+string uuid();
 
+P_Node push_back(P_Node const &head, int data);
+P_Node push_front(P_Node const &head, int data);
+P_Node push_at(P_Node const &head, int data, int pos);
+P_Node push_in_order(P_Node const &head, int data, bool asc = true);
 
-P_Node push_back(P_Node const &head, int dato);
-P_Node push_front(P_Node const &head, int dato);
-P_Node push_at(P_Node const &head, int dato, int pos);
-P_Node push_in_order(P_Node const &head, int dato, bool asc =  true);
+P_Node pop_front(P_Node const &head);
+P_Node pop_back(P_Node const &head);
 
+P_Node insertBefore(P_Node const &head, int data);
